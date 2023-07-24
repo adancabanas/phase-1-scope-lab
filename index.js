88613ce24902a5c4daec92e14dffa3254adee5d5
Adan Nicolas Cabanas Garza <adan.cabanas@gmail.com>
2023-07-24 13:22:34 -0500

    defines scope lab excercise

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+const leastFavoriteCustomer = '';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(customer) {
+    bestCustomer = customer;
+}
+
+function changeLeastFavoriteCustomer(customer) {
+    leastFavoriteCustomer = customer;
+}
